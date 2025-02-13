e0f1e4d259d62cf71ba7d6516d18fd68f63d3efe
hexuan <hexuan@idea.edu.cn>
2025-02-13 14:16:26 +0800

    fix: support dark mode

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,9 +9,11 @@ export default function PicexLayout({
 	right: ReactNode;
 }>) {
 	return (
-		<section className="flex h-full">
+		<section className="picex flex h-full">
 			<div className="relative w-1/3 max-w-[396px]">{left}</div>
-			<div className="relative min-w-0 flex-1 bg-[#F8FAFC]">{children}</div>
+			<div className="relative min-w-0 flex-1 bg-[#f8fafc] dark:bg-[#ffffff14]">
+				{children}
+			</div>
 			<div className="relative max-w-1/5 flex-[0_1_0]">{right}</div>
 		</section>
 	);
--- src/tools/background/components/Colours.tsx
+++ src/tools/background/components/Colours.tsx
@@ -193,6 +193,7 @@ function ColourItem({
 			style={{
 				border: border && hex !== colour ? `1px solid ${border}` : undefined,
 				background: `url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAMUlEQVQ4T2NkYGAQYcAP3uCTZhw1gGGYhAGBZIA/nYDCgBDAm9BGDWAAJyRCgLaBCAAgXwixzAS0pgAAAABJRU5ErkJggg==)`,
+				backgroundColor: hex === 'transparent' ? '#fff' : undefined,
 			}}
 			onClick={() => {
 				setHex(colour);
